94336586afd4584945707f7b666ea0953ed0b527
Agbebi <agbebitimothy8@gmail.com>
2025-06-12 08:47:14 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-for (let i = 0; i < document.querySelectorAll("span").length; i++) {
-    document.querySelector(".hamburger").addEventListener("click", function () {
-        document.querySelectorAll("span")[i].classList.toggle("active");
-        document.querySelector("div.menu-bar").classList.toggle("active");
-    })
-}
-
-
